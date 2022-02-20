1aa7fb27efae22a7fc350134b97ceac2c5307ac9
yangxinprops <yx1164352008>
2022-02-20 22:46:10 +0800

    ''

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config) {
     const instance = axios.create({
-      baseURL:'http://152.136.185.210:7878/api/hy66',
+      baseURL:'填自己的地址，或找老师要',
       timeout:5000
     })
 
